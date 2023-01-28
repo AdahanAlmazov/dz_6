9e4c423bb906648d68944e4f1cf3925990a397b6
popensislam <s.islam.like@mail.ru>
2023-01-28 21:01:59 +0500

    lesson 5

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import TodoCard from "./TodoCard"
 
 import classes from './components.module.css'
+import { Context } from '../App'
+import { useSearch, useSort } from './hooks'
 
-const TodoList = ({ todoList, editTodo, deleteTodo, completedOnChange }) => {
+const TodoList = ({ type, todoList, offset, editTodo, deleteTodo, completedOnChange }) => {
+  const { search } = useContext(Context);
+
+  const result = useSearch(todoList, search, 'title')
+
+  const sortedArray = useSort(result, type, 'date', 'title')
+
+  if ( sortedArray?.length === 0 && search ) {
+    return (
+      <p>По данному <strong>{search}</strong> запросу ничего не найдено</p>
+    )
+  }
 
   return (
     <div className={classes.flexList}>
-      {todoList.map((todo, i) => 
+      {sortedArray?.slice(offset, offset + 2).map((todo, i) => 
         <TodoCard key={todo.date} todo={todo} editTodo={editTodo} deleteTodo={deleteTodo} completedOnChange={completedOnChange}/>
       )}
     </div>
